eefa161ffdd85d0a475ab1b90c6b8254e3cfd9c4
Marius Vorster <marius.vrstr@gmail.com>
2022-04-05 11:49:51 +0200

    Added basic project structure

---
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -0,0 +1,9 @@
+﻿namespace Inoxico.TechnicalQuestions.Answers;
+
+public class QuestionOne
+{   
+    public static int GetLongestSentance(string s)
+    {
+        throw new NotImplementedException();
+    }
+}
